e0a75045e04b0ffc071d2fbc5aab3dbf49f2ebed
José Victor <66651520+jvscursulim@users.noreply.github.com>
2022-12-17 17:08:39 +0000

    update on train.py

--- script/train.py
+++ script/train.py
@@ -2,13 +2,16 @@ import tensorflow as tf
 
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.lite import tflite
 from keras import layers
 
 datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
-dataset_path = ""
+# Dataset link: https://www.kaggle.com/datasets/arunrk7/surface-crack-detection
+# Download the dataset and put the folders with the images of the dataset in data/dataset
+dataset_path = "../data/dataset"
 target_size = (150, 150)
-batch_size = 20
+batch_size = 64
 epochs = 5
 learning_rate = 0.001
 optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
@@ -39,3 +42,11 @@ model.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
 
 model.fit(train_dataset, epochs=epochs, validation_data=validation_dataset)
 
+lr_str = str(learning_rate).replace(".", "_")
+model.save(f"../model/cnn_surface_crack_detection_bs{batch_size}_epochs{epochs}_lr_{lr_str}.h5")
+
+converter = tflite.TFLiteConverter.from_keras_model(model=model)
+tflite_model = converter.convert()
+
+with open("../model/cnn_surface_crack_detection_bs64_epochs5_lr_0_001.tflite", "wb") as file:
+    file.write(tflite_model)
